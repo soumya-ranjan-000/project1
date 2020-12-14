dfaf6a488ecbcf347adcacce44ae01dc0242ba07
soumya-ranjan-000 <58003666+soumya-ranjan-000@users.noreply.github.com>
2020-12-14 20:26:09 +0530

    navbar design
    make responsive and add open and close button

--- index.js
+++ index.js
@@ -1,5 +1,22 @@
 
-function hide(){
-    document.getElementsByClassName('navigation-menu')[0].classList.toggle('hide');
-    // document.getElementById('section-1').style.opacity="0.1"
+const open =document.getElementById('open');
+const close=document.getElementById('close');
+const menu=document.querySelector('.section-1 .navigation-menu ul');
+
+open.addEventListener('click',showMenu);
+close.addEventListener('click',closeMenu);
+
+function showMenu(){
+    open.style.display="none";
+    close.style.display="inline-block";
+    menu.style.display="flex";
+    menu.style.top="-2vh";
+    
+}
+
+function closeMenu(){
+    open.style.display="inline-block";
+    menu.style.top="-102vh";
+    // menu.style.display="none";
+    close.style.display="none";
 }
